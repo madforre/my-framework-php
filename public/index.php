9e89096800eef40974834a3995b27a184e62ad72
Madforre <33324825+madforre@users.noreply.github.com>
2018-09-14 19:02:53 +0900

    git 클론코딩 + flex 정리
    주말 목표 - flex 숙달

---
+++ public/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/app.css">
+    <title>Git-hub layouts</title>
+</head>
+<body>
+<div id="wrap">
+    <div class="nav">
+        <ul class="gnb">
+            <li>로고</li>
+            <li><a href="">Features</a></li>
+            <li><a href="">Business</a></li>
+            <li><a href="">Explore</a></li>
+            <li><a href="">Marketplace</a></li>
+            <li><a href="">Pricing</a></li>
+            <li><input type="text" placeholder="Search GitHub"></li>
+            <li>Sign in or Sign up</li>
+        </ul>
+    </div>
+    <div class="section one">
+        <div class="left">
+            <h1>Built for <br> developers</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint expedita corrupti excepturi corporis porro dolorem aliquam veritatis perspiciatis dolorum incidunt blanditiis ipsa nemo dignissimos eius cupiditate beatae laboriosam harum magni, ex id optio non obcaecati exercitationem! Odio dicta qui reprehenderit.</p>
+        </div>
+        <div class="right">
+        </div>
+
+
+    </div>
+    <div class="section two">
+        <h1>Try GitHub Enterprise</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis facere iusto labore tempora? Nulla error aliquid, reprehenderit dolores quidem quibusdam ea modi sint esse dolore ipsum qui, accusamus iusto. Repellat.</p>
+        <button class="blue">ipsum dolor.</button>
+    </div>
+    <div class="section three">
+        <h1>A better way to work together</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, rem ducimus repudiandae reprehenderit consequatur saepe voluptate sint odit unde quibusdam. Obcaecati optio unde exercitationem rem. Officiis facere exercitationem saepe nisi.</p>
+        <button class="white">Lorem, ipsum dolor.</button>
+    </div>
+    <div class="section four">
+        <h1>Boxes? Check.</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic dolorum ipsa maiores velit! Nemo labore molestiae at. Et, quisquam magni! Veritatis perspiciatis labore praesentium nesciunt reiciendis rem, quae dolores repellat illo consequuntur veniam adipisci exercitationem, sed hic commodi iusto ex.</p>
+        <button class="white">Lorem, ipsum dolor.</button>
+    </div>
+    <div class="section five">
+        <h1>Build on GitHub</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim incidunt suscipit obcaecati esse voluptatum aperiam reprehenderit doloremque a eum voluptate quaerat dicta, veniam soluta odit quidem autem magnam quia, ipsa debitis vel officia facilis possimus. Nulla aspernatur distinctio repellat dolorem.</p>
+        <button class="white">Lorem, ipsum dolor.</button>
+    </div>
+    <div class="section six">
+        <h1>Welcome home,<br> developers</h1
+        ><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus enim consequuntur natus debitis esse vero tempore neque unde quae at.</p>
+    </div>
+    <div class="section seven">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus enim consequuntur natus debitis esse vero tempore neque unde quae at.</p>
+    </div>
+    <div class="section eight">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus enim consequuntur natus debitis esse vero tempore neque unde quae at.</p>
+    </div>
+    <div class="footer"></div>
+</div>
+</body>
+</html>
